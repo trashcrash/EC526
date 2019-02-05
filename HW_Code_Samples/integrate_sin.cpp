42e6f68cd1f1914515eeb6112dcef943fa129a68
brower <brower@bu.edu>
2019-02-05 12:24:38 -0500

    Update Codes Samples for HW2

--- HW_Code_Samples/integrate_sin.cpp
+++ HW_Code_Samples/integrate_sin.cpp
@@ -1,10 +1,12 @@
-
 #include <cmath>
 #include <stdio.h>
 #include <chrono>
 
 int main(int argc, char** argv)
 {
+ FILE *outfile;
+ outfile = fopen("scaling.dat","w");
+
   // Beginning of interval 
   double a = 1.0;
 
@@ -24,13 +26,13 @@ WARNING: As an experiment I am apparently pushing this example
  beyond the ability of double precesion. Why? Not sure!
 *********************/
   
-  for(int Nintervals; Nintervals < 16; Nintervals++)
+  for(int Nintervals; Nintervals < 15; Nintervals++)
     { N = 4*N;
     
   // Perform the integral
        // Pre-compute the window width.
   double h = (b-a)/((double)N);
-   double sum = 0.0;
+  double sum = 0.0;
 
    // TIMING LINE 1: Get the starting timestamp. 
   std::chrono::time_point<std::chrono::steady_clock> begin_time =
@@ -50,9 +52,23 @@ WARNING: As an experiment I am apparently pushing this example
   double difference_in_seconds = difference_in_time.count();
 
   // Print the integral.
-  printf("The integral from %.8f to %.8f of sin(x) using %d intervals is %.15f and error is %.10e  \n", a, b, N, sum, exact - sum);
-  printf("This took %.8f seconds.\n", difference_in_seconds);
-    }
+  printf("The integral from %.8f to %.8f of sin(x) using %d intervals is %.15f and error is %.10e  \n", a, b, N, sum,  abs(exact - sum));
+
+  /***********************
+
+Simple example of printing columns of data to file that is freiendly to
+later gnuplot or Mathematica Plotting!  Of course you  will want to
+restructure this according to what you want to plot but because
+both plotting can easily select columns and rages it is a very flexible.
+
+***********************/
+ printf("This took %.8f seconds.\n", difference_in_seconds);
+   
+ fprintf(outfile,"  %.8f  %.8f  %d  %.15f  %.10e    %.8f \n", a, b, N, sum, abs(exact - sum), difference_in_seconds);
+   
+ }
+
+  fclose(outfile);
   
   return 0;
 }
